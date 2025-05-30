1b26bca46a9a4614dfd2e1ccf7873a154804ac3e
walidsardi <walid.sardi@memorandum.es>
2025-05-30 12:33:17 +0200

    Cambios

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,11 @@
 import './Footer.css'
 
-const Footer = () => <footer className='footer'></footer>
+const Footer = () => (
+  <footer className='footer'>
+    <a href='https://github.com/walidsardib' className='link footer__link'>
+      Created By Walid Sardi
+    </a>
+  </footer>
+)
 
 export default Footer
